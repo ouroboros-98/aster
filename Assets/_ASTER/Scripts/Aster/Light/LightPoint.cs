eb852ea539074fdd6c3427ae3d3c7a3f1a70410c
AmitAizenman <amitaizenman2@gmail.com>
2025-04-20 20:21:58 +0300

    added LightPoint class

---
+++ Assets/_ASTER/Scripts/Aster/Light/LightPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Aster.Core;
+using UnityEngine;
+
+namespace Aster.Light
+{
+    public class LightPoint : AsterMono
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+           //todo: finish this 
+        }
+    }
+}
