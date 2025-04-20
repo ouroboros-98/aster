7b491787b012e7783688c2b8ea02e204816c197e
AmitAizenman <amitaizenman2@gmail.com>
2025-04-20 22:25:06 +0300

    added DummyLightPoint and updated LightPoint Script

--- Assets/_ASTER/Scripts/Aster/Light/LightPoint.cs
+++ Assets/_ASTER/Scripts/Aster/Light/LightPoint.cs
@@ -1,14 +1,51 @@
 ﻿using System;
-using Aster.Core;
 using UnityEngine;
 
 namespace Aster.Light
 {
-    public class LightPoint : AsterMono
+    public class LightPoint : MonoBehaviour
     {
-        private void OnTriggerEnter(Collider other)
+        [SerializeField] private float speed = 5f;
+        [SerializeField] private Transform lightSource;
+        [SerializeField] private Rigidbody rb;
+        [SerializeField] private float arrivalThreshold = 0.1f;
+
+        public event Action OnArrivedAtLightSource;
+
+        private bool _isMovingToLightSource = false;
+
+        private void OnCollisionEnter(Collision other)
         {
-           //todo: finish this 
+            if (other.gameObject.CompareTag("Player"))
+            {
+                StartMovingToLightSource();
+            }
+        }
+
+        private void StartMovingToLightSource()
+        {
+            _isMovingToLightSource = true;
+            rb.isKinematic = true; // So physics doesn't interfere
+        }
+
+        private void Update()
+        {
+            if (_isMovingToLightSource)
+            {
+                Vector3 direction = (lightSource.position - transform.position).normalized;
+                transform.position += direction * (speed * Time.deltaTime);
+
+                if (Vector3.Distance(transform.position, lightSource.position) < arrivalThreshold)
+                {
+                    ArriveAtLightSource();
+                }
+            }
+        }
+
+        private void ArriveAtLightSource()
+        {
+            OnArrivedAtLightSource?.Invoke();
+            Destroy(gameObject); 
         }
     }
 }
