4f6220832a92eb9e7465c3482769b156079d08c6
NadavIshaak <nako354@gmail.com>
2025-04-20 17:38:03 +0300

    wavemanager and enemy spawners

---
+++ Assets/_ASTER/Scripts/Aster/Core/InputSystem/WaveManager.cs
@@ -0,0 +1,58 @@
+﻿using _ASTER.Scripts.Aster.Entity.Enemy;
+
+namespace Aster.Core.InputSystem
+{
+    using System.Collections;
+    using UnityEngine;
+
+    public class WaveManager : MonoBehaviour
+    {
+        [SerializeField] private EnemySpawner[] spawners;
+        [SerializeField] private float initialSpawnInterval = 5f;
+        [SerializeField] private int initialEnemies = 5;
+        [SerializeField] private float difficultyFactor = 0.9f;
+        [SerializeField] private int additionalEnemiesPerWave = 2;
+
+        private WaveData currentWave = new WaveData();
+
+        private void Start()
+        {
+            currentWave.WaveNumber = 1;
+            currentWave.EnemiesToSpawn = initialEnemies;
+            currentWave.SpawnInterval = initialSpawnInterval;
+            StartCoroutine(SpawnWaves());
+        }
+
+        private IEnumerator SpawnWaves()
+        {
+            while (true)
+            {
+                yield return StartCoroutine(SpawnWave());
+                currentWave.WaveNumber++;
+                currentWave.EnemiesToSpawn += additionalEnemiesPerWave;
+                currentWave.SpawnInterval *= difficultyFactor;
+            }
+        }
+
+        private IEnumerator SpawnWave()
+        {
+            int enemiesLeft = currentWave.EnemiesToSpawn;
+
+            // Pick a random starting index
+            int startIndex = Random.Range(0, spawners.Length);
+
+            // Choose up to three consecutive spawners
+            int endIndex = Mathf.Min(startIndex + 2, spawners.Length - 1);
+
+            while (enemiesLeft > 0)
+            {
+                for (int i = startIndex; i <= endIndex && enemiesLeft > 0; i++)
+                {
+                    spawners[i].Spawn();
+                    enemiesLeft--;
+                    yield return new WaitForSeconds(currentWave.SpawnInterval / currentWave.EnemiesToSpawn);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_ASTER/Scripts/Aster/Entity/Enemy/EnemySpawner.cs
@@ -0,0 +1,14 @@
+﻿using Aster.Core;
+using UnityEngine;
+
+namespace _ASTER.Scripts.Aster.Entity.Enemy
+{
+    public class EnemySpawner : AsterMono
+    {
+        [SerializeField] private GameObject _enemyPrefab;
+        public void Spawn()
+        {
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
