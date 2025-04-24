d3b4d3ac69b206c45c4f664b4ddd379a60d25d80
ouroboros-98 <dolev.irron@gmail.com>
2025-04-24 13:50:04 +0300

    Extend EnemySpawner

--- Assets/_ASTER/Scripts/Aster/Entity/Enemy/EnemySpawner.cs
+++ Assets/_ASTER/Scripts/Aster/Entity/Enemy/EnemySpawner.cs
@@ -1,4 +1,8 @@
-﻿using Aster.Core;
+﻿using System;
+using Aster.Core;
+using Aster.Entity.Enemy;
+using Aster.Light;
+using Aster.Utils;
 using Aster.Utils.Pool;
 using UnityEngine;
 
@@ -6,11 +10,32 @@ namespace _ASTER.Scripts.Aster.Entity.Enemy
 {
     public class EnemySpawner : AsterMono
     {
+        [SerializeField] private float spawnDistance;
+
+        private Transform              mainLightSource;
+        private IPool<EnemyController> _enemyPool;
+
+        private void Awake()
+        {
+            _enemyPool      = EnemyPool.Instance;
+            mainLightSource = MainLightSource.Instance.transform;
+        }
+
+
         public void Spawn()
         {
             var enemy = EnemyPool.Instance.Get();
             enemy.transform.position = transform.position;
             enemy.transform.rotation = transform.rotation;
+        }
+
+        public void SpawnEnemy(Angle direction)
+        {
+            Vector3 spawnDirection = direction.ToVector3();
+            Vector3 spawnPosition  = mainLightSource.position + (spawnDistance * spawnDirection);
+
+            var enemy = EnemyPool.Instance.Get();
+            enemy.transform.position = spawnPosition;
         }
     }
 }
