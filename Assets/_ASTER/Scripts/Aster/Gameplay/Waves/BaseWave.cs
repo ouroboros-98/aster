f4707add7430ba51b734452a68140c61206168a3
AmitAizenman <amitaizenman2@gmail.com>
2025-04-24 13:44:52 +0300

    added TuturialWave Logic

--- Assets/_ASTER/Scripts/Aster/Gameplay/Waves/BaseWave.cs
+++ Assets/_ASTER/Scripts/Aster/Gameplay/Waves/BaseWave.cs
@@ -1,3 +1,4 @@
+using _ASTER.Scripts.Aster.Entity.Enemy;
 using UnityEngine;
 
 namespace Aster.Gameplay.Waves
@@ -5,6 +6,12 @@ namespace Aster.Gameplay.Waves
     [CreateAssetMenu(fileName = "BaseWave", menuName = "Scriptable Objects/BaseWave")]
     public abstract class BaseWave : ScriptableObject
     {
+        protected EnemySpawner EnemySpawner;
+
+        public void Init(EnemySpawner enemySpawner)
+        { 
+            EnemySpawner = enemySpawner;
+        }
         public abstract void OnWaveStart();
         public abstract void OnWaveEnd();
     }
