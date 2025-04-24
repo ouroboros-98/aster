ed04927bdbcf17b0a6de0844c8c95c56172f6224
ouroboros-98 <dolev.irron@gmail.com>
2025-04-24 12:48:45 +0300

    Implement LightSphereCaster.cs

---
+++ Assets/_ASTER/Scripts/Aster/Light/LightSphereCaster.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Aster.Utils;
+using UnityEngine;
+
+namespace Aster.Light
+{
+    public class LightSphereCaster : ILightCaster
+    {
+        public List<LightHit> GetHits(ILightRay ray)
+        {
+            List<LightHit> result = new();
+
+            RaycastHit[] hits = Physics.SphereCastAll(ray.Origin, ray.Width / 2f, ray.Direction);
+
+            Debug.DrawRay(ray.Origin, ray.Direction * LightRay.MAX_DISTANCE, Color.red, 0.005f);
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (!hit.collider.ScanForComponent(out BaseLightHittable hittable, parents: true,
+                                                   children: true))
+                    continue;
+
+                result.Add(new(ray, hit.point, hittable));
+            }
+
+            result.Sort((x, y) => x.Distance.CompareTo(y.Distance));
+
+            return result;
+        }
+    }
+}
