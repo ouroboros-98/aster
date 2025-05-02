9cd55a37a9da9260b3b2dc3ccbf7508ee19939fe
NadavIshaak <nako354@gmail.com>
2025-05-02 15:02:52 +0300

    onInteract indicator pop up development

---
+++ Assets/_ASTER/Scripts/Aster/UI/PopUpIndicator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Aster.Core.Entity;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+
+namespace Aster.Core.UI
+{
+    public class PopUpIndicator : AsterMono
+    {
+        private LookAtConstraint _lookAt;
+
+        [ShowNonSerializedField] private Transform _cameraAimPoint;
+
+        [SerializeField] private Image indicatorImage;
+
+        private void Awake()
+        {
+            Reset();
+            
+
+            if (_cameraAimPoint == null) _cameraAimPoint = AsterCamera.Instance.AimPoint;
+
+            _lookAt.AddSource(
+                new ConstraintSource
+                {
+                    sourceTransform = _cameraAimPoint,
+                    weight          = 1
+                }
+            );
+        }
+        
+
+        private void Reset()
+        {
+            ValidateComponent(ref _lookAt);
+        }
+    }
+}
