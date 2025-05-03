f13e0fa3cfc2ebb42e54b95089cbd510ee95f645
AmitAizenman <amitaizenman2@gmail.com>
2025-05-03 18:12:02 +0300

    added towerAdderManager

---
+++ Assets/_ASTER/Scripts/Aster/Core/TowerAdderManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Aster.Core
+{
+    public class TowerAdderManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject towerToAdd;
+        [SerializeField] private Transform placeToAdd;
+        [SerializeField] private int[] wavesToDrop;
+        private int _waveCounter;
+        private bool _needToAdd;
+        private bool _placeToAddEmpty;
+        private void OnEnable()
+        {
+            AsterEvents.Instance.OnWaveStart += IncrementCounter;
+            AsterEvents.Instance.OnTowerPlacedBetween += TowerPlaced;
+            AsterEvents.Instance.OnTowerRemovedBetween += TowerRemoved;
+        }
+
+        private void OnDisable()
+        {
+            AsterEvents.Instance.OnWaveStart -= IncrementCounter;
+            AsterEvents.Instance.OnTowerPlacedBetween -= TowerPlaced;
+            AsterEvents.Instance.OnTowerRemovedBetween -= TowerRemoved
+        }
+
+        private void IncrementCounter(int obj)
+        {
+            _waveCounter++;
+            if (wavesToDrop.Contains(_waveCounter))
+            {
+                _needToAdd = true;
+                AddTower();
+            }
+        }
+
+        private void AddTower()
+        {
+            if (_placeToAddEmpty && _needToAdd)
+            {
+                Instantiate(towerToAdd, placeToAdd.position, quaternion.identity);
+            }
+        }
+
+        private void TowerPlaced()
+        {
+            _placeToAddEmpty = false;
+        }
+
+        private void TowerRemoved()
+        {
+            _placeToAddEmpty = true;
+        }
+    }
+}
