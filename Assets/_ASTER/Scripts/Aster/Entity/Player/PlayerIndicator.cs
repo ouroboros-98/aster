129b3ce9142fd6e9cd6ff4df644da0edeae37b2c
NadavIshaak <nako354@gmail.com>
2025-05-04 19:33:45 +0300

    indicator revamp

---
+++ Assets/_ASTER/Scripts/Aster/Entity/Player/PlayerIndicator.cs
@@ -0,0 +1,22 @@
+﻿using Aster.Core;
+using Aster.Core.UI;
+using UnityEngine;
+
+namespace Aster.Entity.Player
+{
+    public class PlayerIndicator:AsterMono
+    {
+        [SerializeField] private PopUpIndicator indicator;
+        
+        private void Awake()
+        {
+            indicator.SetEnabled(false);
+        }
+        
+        public void SetIndicator(bool enabled)
+        {
+            indicator.SetEnabled(enabled);
+        }
+        
+    }
+}
