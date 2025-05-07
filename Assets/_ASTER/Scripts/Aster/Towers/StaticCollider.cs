04dbfa8450b721612ed017c5e82925af9033430e
NadavIshaak <nako354@gmail.com>
2025-05-07 15:19:30 +0300

    changed emmiter colider to make it useable from near

---
+++ Assets/_ASTER/Scripts/Aster/Towers/StaticCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class StaticCollider : MonoBehaviour
+{
+    private Vector3 _fixedPosition;
+
+    private void Awake()
+    {
+        // remember the world‐space position at start
+        _fixedPosition = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        // force the collider to stay at that world‐space position
+        transform.position = _fixedPosition;
+    }
+}
